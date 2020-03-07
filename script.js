73256e1a0313f0798a45d942441c5727e60a4c2e
Nima Sherpa <ndsherpa@gmail.com>
2020-03-07 14:43:53 +1030

    Update to script.js and index.html (adding in variables and functions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-var passwordfinal = document.querySelector("")
+//Definining variables from the html document
+var passwordfinal = document.querySelector("#finalpassword");
+var characterlength = document.querySelector("#length");
+var lowercase = document.querySelector("#lowercase");
+var uppercase = document.querySelector("#uppercase");
+var numeric = document.querySelector("#numeric");
+var special = document.querySelector("#special");
+var form = document.querySelector("#form");
+
+//Response to the form submit button
+form.addEventListener("submit",function(){
+    event.preventDefault();
+    
+}
